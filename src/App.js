2e77645f006980d962224bd0057db820c591f6c0
remmouche <ahsene.remmouche@gmail.com>
2023-08-17 20:44:42 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="title">BISMI ALLAH</h1>
+
+            <div className="header__logo"></div>
+
+            <div className="header__search"></div>
+
+            <div className="header__nav">
+                <div className="nav__item"></div>
+                <div className="nav__item"></div>
+                <div className="nav__item"></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
